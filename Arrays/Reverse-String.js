e8dac86daf047880f2d535a2d2c0a3c3612f586a
Nikhil Randhawa <ranveersingh@Ranveers-MacBook.local>
2025-05-21 09:09:16 +0530

    Reverse a String

---
+++ Arrays/Reverse-String.js
@@ -0,0 +1,8 @@
+// Hello --> olleh
+// steps--> 1. split the string
+//          2. reverse the string
+//          3. join the string
+
+const reverseString = (str) => str.split("").reverse().join("");
+
+console.log(reverseString("hello brother"));
